6fd5a7d039a297c4f5b688559a069fb9230dacbd
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2024-03-18 02:15:27 +0200

    DRY code for order operation in the various payment methods

--- Model/DiscoveryMiles.php
+++ Model/DiscoveryMiles.php
@@ -11,17 +11,8 @@
 
 namespace PayU\EasyPlus\Model;
 
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Payment\Model\InfoInterface;
-use Magento\Sales\Api\Data\OrderPaymentInterface;
-use Magento\Sales\Model\Order\Payment\Transaction;
-use Magento\Quote\Model\Quote;
-
 /**
  * Payment model for payment method DiscoveryMiles
- *
- * @SuppressWarnings(PHPMD.TooManyFields)
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class DiscoveryMiles extends AbstractPayment
 {
@@ -33,27 +24,4 @@ class DiscoveryMiles extends AbstractPayment
      * @var string
      */
     protected $_code = self::CODE;
-
-    /**
-     * Order payment
-     *
-     * @param InfoInterface| Payment $payment
-     * @param float $amount
-     * @return $this
-     * @throws LocalizedException
-     */
-    public function order(InfoInterface $payment, $amount)
-    {
-        $payURedirect = $this->_session->getCheckoutRedirectUrl();
-        if (!$payURedirect) {
-            return $this->_setupTransaction($payment, $amount);
-        }
-
-        $payment->setSkipOrderProcessing(true);
-
-        $payment->getOrder()->setState(\Magento\Sales\Model\Order::STATE_PENDING_PAYMENT)
-            ->setStatus(\Magento\Sales\Model\Order::STATE_PENDING_PAYMENT);
-
-        return $this;
-    }
 }
--- Model/MobileBanking.php
+++ Model/MobileBanking.php
@@ -11,14 +11,8 @@
 
 namespace PayU\EasyPlus\Model;
 
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Payment\Model\InfoInterface;
-
 /**
  * Payment model for payment method MobileBanking
- *
- * @SuppressWarnings(PHPMD.TooManyFields)
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class MobileBanking extends AbstractPayment
 {
@@ -30,27 +24,4 @@ class MobileBanking extends AbstractPayment
      * @var string
      */
     protected $_code = self::CODE;
-
-    /**
-     * Order payment
-     *
-     * @param InfoInterface| Payment $payment
-     * @param float $amount
-     * @return $this
-     * @throws LocalizedException
-     */
-    public function order(InfoInterface $payment, $amount)
-    {
-        $payURedirect = $this->_session->getCheckoutRedirectUrl();
-        if (!$payURedirect) {
-            return $this->_setupTransaction($payment, $amount);
-        }
-
-        $payment->setSkipOrderProcessing(true);
-
-        $payment->getOrder()->setState(\Magento\Sales\Model\Order::STATE_PENDING_PAYMENT)
-            ->setStatus(\Magento\Sales\Model\Order::STATE_PENDING_PAYMENT);
-
-        return $this;
-    }
 }
